5f5408b8ae9d4b97a9e2f3127ad36048ebd55fc9
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-01-06 14:57:05 +0000

    Sync LeetCode submission - Intersection of Two Arrays (python3)

---
+++ my-folder/problems/intersection_of_two_arrays/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        dictionary = {}
+        ans = []
+        for i in nums1:
+            dictionary[i] = 1
+        for j in nums2:
+            if j in dictionary and dictionary[j]!= 0:
+                dictionary[j] -= 1
+                ans.append(j)
+        return ans
