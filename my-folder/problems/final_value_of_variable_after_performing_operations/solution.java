11caa622edd11632a5b4d24604d39a5cd9d0d943
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-06-11 15:33:22 +0000

    Sync LeetCode submission - Final Value of Variable After Performing Operations (java)

---
+++ my-folder/problems/final_value_of_variable_after_performing_operations/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int value = 0;
+        
+        for(String s: operations){
+            if(s.charAt(0) == '+' || s.charAt(1) == '+') {value ++;}
+            else{value --;}
+        }
+        return value;
+    }
+}
