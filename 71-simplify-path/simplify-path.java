a21f4eacdc5bc7485f4747879d1f6f14f8585cd1
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-08-25 09:59:38 +0530

    Time: 12 ms (9.11%) | Memory: 44.4 MB (24.94%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,28 +1,50 @@
 class Solution {
     public String simplifyPath(String path) {
         // Bruteforce: O(n)
+//         String[] components = path.split("/+");
+//         List<String> directories = new ArrayList<>();
+        
+//         System.out.println(Arrays.deepToString(components));
+//         for(String c: components){
+//             if(c.equals(".") || c.isEmpty())
+//                 continue;
+//             else if(c.equals("..")){
+//                 if(!directories.isEmpty())
+//                     directories.remove(directories.size() - 1);
+//             } else
+//                 directories.add(new String(c));
+//         }
+//         System.out.println(directories);
+//         StringBuilder ans = new StringBuilder("/");
+//         for(int i = 0; i < directories.size(); i ++){
+//             ans.append(directories.get(i));
+//             if(i < directories.size() - 1)
+//                 ans.append("/");
+//         }
+        
+//         return ans.toString();
+        
+        // Optimal using Stack: O(n)
         String[] components = path.split("/+");
-        List<String> directories = new ArrayList<>();
+        Stack<String> st = new Stack<>();
         
         System.out.println(Arrays.deepToString(components));
         for(String c: components){
             if(c.equals(".") || c.isEmpty())
                 continue;
             else if(c.equals("..")){
-                if(!directories.isEmpty())
-                    directories.remove(directories.size() - 1);
+                if(!st.isEmpty())
+                    st.pop();
             } else
-                directories.add(new String(c));
+                st.push(new String(c));
         }
-        System.out.println(directories);
-        StringBuilder ans = new StringBuilder("/");
-        for(int i = 0; i < directories.size(); i ++){
-            ans.append(directories.get(i));
-            if(i < directories.size() - 1)
-                ans.append("/");
+        // System.out.println(directories);
+        StringBuilder ans = new StringBuilder();
+        for(String s: st){
+            ans.append("/").append(s);
         }
         
-        return ans.toString();
+        return ans.length() == 0 ? "/" : ans.toString();
         
     }
 }
