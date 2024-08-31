676e95294f7aba94eb83f306fa656f0cf5ff34aa
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-08-31 18:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (81.75%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -16,32 +16,54 @@
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
         // Bruteforce -> TC: O(N), SC: O(w)
+//         List<Integer> res = new ArrayList<>();
+        
+//         if(root == null)
+//             return res;
+        
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.add(root);
+        
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             for(int i = 0; i < size; i ++){
+//                 TreeNode curr = q.poll();
+                
+//                 if(i == size - 1){
+//                     res.add(curr.val);
+//                 }
+                
+//                 if(curr.left != null)
+//                     q.add(curr.left);
+                
+//                 if(curr.right != null)
+//                     q.add(curr.right);
+//             }
+//         }
+        
+//         return res;
+        
+        // Optimal -> TC: O(N), SC: O(levels)
         List<Integer> res = new ArrayList<>();
         
         if(root == null)
             return res;
         
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            for(int i = 0; i < size; i ++){
-                TreeNode curr = q.poll();
-                
-                if(i == size - 1){
-                    res.add(curr.val);
-                }
-                
-                if(curr.left != null)
-                    q.add(curr.left);
-                
-                if(curr.right != null)
-                    q.add(curr.right);
-            }
-        }
+        rightTraversal(root, 0, res);
         
         return res;
         
+    }
+    
+    public void rightTraversal(TreeNode root, int level, List<Integer> res){
+        if(root == null)
+            return;
+        
+        if(level == res.size()){
+            res.add(root.val);
+        }
+        
+        rightTraversal(root.right, level + 1, res);
+        rightTraversal(root.left, level + 1, res);
     }
 }
