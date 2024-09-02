454ae22e324ae3fa6a01ba33474586f661fe1741
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-02 15:36:38 +0530

    Time: 31 ms (57.07%) | Memory: 51.6 MB (48.10%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -29,24 +29,35 @@ class Solution {
         // Optimal solution: O(n^2)
         if(nums.length < 3)
             return new ArrayList<>();
-        
-        Set<List<Integer>> res  = new HashSet<>();
-        // if(nums.length==0) return new ArrayList<>(res);
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i=0; i<nums.length-2;i++){
-            if(i > 0 && nums[i] == nums[i - 1])
+        for(int i = 0; i < nums.length -2 ; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) 
                 continue;
-            int j =i+1;
-           int  k = nums.length-1;
-            while(j<k){
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum==0)res.add(Arrays.asList(nums[i],nums[j++],nums[k--]));
-                else if (sum >0) k--;
-                else if (sum<0) j++;
+            int left = i + 1, right = nums.length - 1;
+            
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    while(left < right && nums[left] == nums[left + 1])
+                            left++;
+                    while(left < right && nums[right] == nums[right - 1])
+                            right --;
+                    
+                    left++;
+                    right--;
+                }else if(sum <0){
+                    left++;
+                }else{
+                    right--;
+                }
             }
-
         }
-        return new ArrayList<>(res);
+        
+        return ans;
     }
         
        
