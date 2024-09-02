90ac054bb933d3a46e9ab59736e753a06f08be43
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-02 15:00:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (39.08%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        // Linear search: O(N)
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == target){
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
