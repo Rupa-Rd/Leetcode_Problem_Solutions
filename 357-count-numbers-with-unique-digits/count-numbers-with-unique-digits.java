938dd106659a5225d502caa74d6158da1c4d81df
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-02 10:42:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (30.41%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -1,15 +1,30 @@
 class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        int count = 0, total = (int)Math.pow(10, n);
+        // Time complexity: O(n * logN)
+//         int count = 0, total = (int)Math.pow(10, n);
         
-        for(int i = 0; i < total; i++){
-            if(checkUnique(i)){
-                count++;
-            }
+//         for(int i = 0; i < total; i++){
+//             if(checkUnique(i)){
+//                 count++;
+//             }
+//         }
+        
+//         return count;
+        // Optimal: O(N)
+        if(n ==0) return 1;
+        
+        int count = 10; // Unit digits 0...10
+        int uniqueDigits = 9, availableDigits = 9;
+        
+        for(int i = 2; i <= n && availableDigits > 0; i++){
+            uniqueDigits *= availableDigits;
+            count += uniqueDigits;
+            availableDigits --;
         }
         
         return count;
     }
+    // Bruteforce: O(N)
     public boolean checkUnique(int n){
         boolean[] digits = new boolean[10];
         
