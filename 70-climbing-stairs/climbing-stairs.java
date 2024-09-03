945da277f1d78115234bd8a6c4a75434cc3f4fc5
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-03 21:53:40 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (38.57%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -2,11 +2,22 @@ class Solution {
     public int climbStairs(int n) {
         // Recursion : O(2 ^ n)
         // Modifying code to do memoization
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        int ans = nWays(n, dp);
+
+//         int[] dp = new int[n + 1];
+//         Arrays.fill(dp, -1);
+//         int ans = nWays(n, dp);
         
-        return ans;
+//         return ans;
+        // Space optimization
+        int prev1 = 1, prev2 = 1,curr = 1;
+        
+        for(int i = 2; i <= n; i++){
+            curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return curr;
     }
     public int nWays(int n, int[] dp){
         if (n == 0){
