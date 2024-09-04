6f7f71b5b426806f54cb10097cb342c592c87248
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-04 09:02:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (24.40%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -10,27 +10,51 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
+        // Bruteforce using: O(N logN)
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
         
-        while(list1 != null){
-            pq.add(list1);
-            list1 = list1.next;
-        }
+//         while(list1 != null){
+//             pq.add(list1);
+//             list1 = list1.next;
+//         }
         
-        while(list2 != null){
-            pq.add(list2);
-            list2 = list2.next;
-        }
-        ListNode newHead = new ListNode(0);
-        ListNode temp = newHead;
+//         while(list2 != null){
+//             pq.add(list2);
+//             list2 = list2.next;
+//         }
+//         ListNode newHead = new ListNode(0);
+//         ListNode temp = newHead;
         
-        while(!pq.isEmpty()){
-            temp.next = pq.remove();
+//         while(!pq.isEmpty()){
+//             temp.next = pq.remove();
+//             temp = temp.next;
+//         }
+        
+//         temp.next = null;
+//         return newHead.next;
+        
+        // Optimal solution: O(N)
+        ListNode head = new ListNode(0);
+        ListNode temp = head;
+        
+        while(list1 != null && list2 != null){
+            if(list1.val <= list2.val){
+                temp.next = list1;
+                list1 = list1.next;
+            }else{
+                temp.next = list2;
+                list2 = list2.next;
+            }
             temp = temp.next;
         }
         
-        temp.next = null;
-        return newHead.next;
+        if(list1 != null){
+            temp.next = list1;
+        }else{
+            temp.next = list2;
+        }
+        
+        return head.next;
     
 //         if(list1 == null)
 //             return list2;
