fe0c04fbe4ea480dd35d2b25566d6cd23af57452
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-04 09:45:22 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (55.06%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,25 +10,62 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        // Bruteforce: O(n * k + logn)
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        // Bruteforce: O(n logn)
+//         PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
         
-        for(ListNode temp: lists){
-            while(temp != null){
-                pq.add(temp);
-                temp = temp.next;
-            }
-        }
+//         for(ListNode temp: lists){
+//             while(temp != null){
+//                 pq.add(temp);
+//                 temp = temp.next;
+//             }
+//         }
         
+//         ListNode head = new ListNode(-1);
+//         ListNode dummy = head;
+        
+//         while(!pq.isEmpty()){
+//             dummy.next = pq.remove();
+//             dummy = dummy.next;
+//         }
+        
+//         dummy.next = null;
+//         return head.next;
+        // Optimal solution: O(n Log k)
+        if(lists == null || lists.length == 0)
+            return null;
+        return mergeLists(lists, 0, lists.length - 1);
+    }
+    public ListNode mergeLists(ListNode[] lists, int left, int right){
+        if(left == right)
+            return lists[left];
+        int mid = left + (right - left) / 2;
+        
+        ListNode l1 = mergeLists(lists, left, mid);
+        ListNode l2 = mergeLists(lists, mid + 1, right);
+        
+        return merge(l1, l2);
+    }
+    public ListNode merge(ListNode l1, ListNode l2){
         ListNode head = new ListNode(-1);
         ListNode dummy = head;
         
-        while(!pq.isEmpty()){
-            dummy.next = pq.remove();
+        while(l1 != null && l2 != null){
+            if(l1.val <= l2.val){
+                dummy.next = l1;
+                l1 = l1.next;
+            }else{
+                dummy.next = l2;
+                l2 = l2.next;
+            }
             dummy = dummy.next;
         }
         
-        dummy.next = null;
+        if(l1 != null){
+            dummy.next = l1;
+        }if(l2 != null){
+            dummy.next = l2;
+        }
+        
         return head.next;
     }
 }
