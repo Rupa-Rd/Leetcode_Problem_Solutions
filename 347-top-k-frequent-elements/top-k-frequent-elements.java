46e6f3087994ea6d8591a0c7c893ccd17d303102
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-05 10:34:56 +0530

    Time: 14 ms (55.85%) | Memory: 45 MB (99.16%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,20 +1,42 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         // Bruteforce: O(N logN)
+//         HashMap<Integer, Integer> freq = new HashMap<>();
+        
+//         for(int i: nums)
+//             freq.put(i, freq.getOrDefault(i, 0) + 1);
+        
+//         List<Map.Entry<Integer, Integer>> sortedList = new ArrayList<>(freq.entrySet());
+//         sortedList.sort((b, a) -> a.getValue() - b.getValue());
+        
+//         System.out.println(sortedList);
+//         int[] res = new int[k];
+//         for(int i = 0; i< k; i++){
+//             res[i] = sortedList.get(i).getKey();
+//         }
+        
+//         return res;
+        
+        // Optimal solution: O(N logK)
         HashMap<Integer, Integer> freq = new HashMap<>();
         
         for(int i: nums)
             freq.put(i, freq.getOrDefault(i, 0) + 1);
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
         
-        List<Map.Entry<Integer, Integer>> sortedList = new ArrayList<>(freq.entrySet());
-        sortedList.sort((b, a) -> a.getValue() - b.getValue());
-        
-        System.out.println(sortedList);
-        int[] res = new int[k];
-        for(int i = 0; i< k; i++){
-            res[i] = sortedList.get(i).getKey();
+        for(Map.Entry<Integer, Integer> e: freq.entrySet()){
+            pq.add(e);
+            if(pq.size() > k){
+                pq.poll();
+            }
         }
+        System.out.println(pq);
         
+        int[] res = new int[k];
+        int i = 0;
+        while(!pq.isEmpty()){
+            res[i++] = pq.remove().getKey();
+        }
         return res;
     }
 }
