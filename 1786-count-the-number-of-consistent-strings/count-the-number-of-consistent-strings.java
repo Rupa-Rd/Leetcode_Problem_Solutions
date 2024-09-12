032e22293ee46c747ec4a021861ed44cc7ce16d1
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-12 07:26:47 +0530

    Time: 17 ms (28.82%) | Memory: 45.3 MB (62.99%) - LeetSync

--- 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
+++ 1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.java
@@ -1,22 +1,54 @@
 class Solution {
     public int countConsistentStrings(String allowed, String[] words) {
-        // Bruteforce using set
-        Set<Character> set = new HashSet<>();
-        for(char c: allowed.toCharArray())
-            set.add(c);
-        
+        // Bruteforce: O(m * n * k)
         int res = 0;
-        for(String s: words){
-            int charCount = 0;
-            for(char ch: s.toCharArray()){
-                if(set.contains(ch)){
-                   charCount++; 
+        for(int i = 0; i < words.length; i++){
+            boolean isConsistent = true;
+            
+            for(int j = 0; j < words[i].length(); j++){
+                char c = words[i].charAt(j) ;
+                boolean isCharConsistent = false;
+                
+                for(int k = 0; k < allowed.length(); k++){
+                    char s = allowed.charAt(k);
+                    
+                    if(s == c){
+                        isCharConsistent =true;
+                        break;
+                    }
                 }
+                if(!isCharConsistent){
+                    isConsistent = false;
+                    break;
+                }
+                    
             }
-            if(charCount == s.length())
+            
+            
+            if(isConsistent)
                 res++;
         }
         
         return res;
+        // Optimized using set: O(m + n *k)
+//         Set<Character> set = new HashSet<>();
+//         for(char c: allowed.toCharArray())
+//             set.add(c);
+        
+//         int res = 0;
+//         for(String s: words){
+//             boolean isConsistent = true;
+            
+//             for(char ch: s.toCharArray()){
+//                 if(!set.contains(ch)){
+//                    isConsistent = false;
+//                     break;
+//                 }
+//             }
+//             if(isConsistent)
+//                 res++;
+//         }
+        
+//         return res;
     }
 }
