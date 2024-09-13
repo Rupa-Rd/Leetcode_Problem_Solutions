d3542fa9272c4019c60301555758412986f98758
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-09-13 11:37:21 +0530

    Time: 2 ms (100.00%) | Memory: 55.2 MB (93.76%) - LeetSync

--- 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
+++ 1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.java
@@ -1,17 +1,44 @@
 class Solution {
     public int[] xorQueries(int[] arr, int[][] queries) {
-        int[] res = new int[queries.length];
-        int k = 0;
+        // Bruteforce: O(N * Q)
+//         int[] res = new int[queries.length];
+//         int k = 0;
         
-        for(int[] q: queries){
-            int l = q[0], r = q[1], ans = arr[l];
+//         for(int[] q: queries){
+//             int l = q[0], r = q[1], ans = arr[l];
             
-            for(int i = l + 1; i <= r; i++){
-                ans ^= arr[i];
-            }
-            res[k++] = ans;
+//             for(int i = l + 1; i <= r; i++){
+//                 ans ^= arr[i];
+//             }
+//             res[k++] = ans;
+//         }
+        
+//         return res;
+        // Better solution with addition space: O(N + Q)
+//         int[] preXOR = new int[arr.length + 1];
+//         int[] res = new int[queries.length];
+//         for(int i = 0; i < arr.length; i++){
+//             preXOR[i + 1] = preXOR[i] ^ arr[i];
+//         }
+        
+//         for(int i = 0; i < queries.length; i++){
+//             res[i] = preXOR[queries[i][1] + 1] ^ preXOR[queries[i][0]];
+//         }
+//         return res;
+        // Optimal solution: O(n + q)
+        int[] res = new int[queries.length];
+        for(int i = 1; i < arr.length; i++){
+            arr[i] = arr[i - 1] ^ arr[i];
         }
         
+        for(int i = 0; i < queries.length; i++){
+            if(queries[i][0] > 0){
+                res[i] = arr[queries[i][1]] ^ arr[queries[i][0] - 1];    
+            }else{
+                res[i] = arr[queries[i][1]];
+            }
+            
+        }
         return res;
     }
 }
