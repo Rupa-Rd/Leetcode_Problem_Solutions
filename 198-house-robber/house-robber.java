2c39520d677ced2421f71b1d5389c6f76b88cf42
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-10-17 09:44:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (16.66%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -5,16 +5,29 @@ class Solution {
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
         // Tabulation: O(N)
-        dp[0] = nums[0];
+        // dp[0] = nums[0];
+        // for(int i = 1; i < n; i++){
+        //     int taken = nums[i];
+        //     if(i > 1)
+        //         taken = nums[i] + dp[i - 2];
+        //     int notTaken = dp[i - 1];
+        //     dp[i] = Math.max(taken, notTaken);
+        // }
+        // return dp[n - 1];
+        // return solve(nums, n - 1, dp);
+        
+        // Space optimized
+        int prev = 0, curr = nums[0];
         for(int i = 1; i < n; i++){
             int taken = nums[i];
             if(i > 1)
-                taken = nums[i] + dp[i - 2];
-            int notTaken = dp[i - 1];
-            dp[i] = Math.max(taken, notTaken);
+                taken = nums[i] + prev;
+            int notTaken = curr;
+            
+            prev = curr;
+            curr = Math.max(taken, notTaken);
         }
-        return dp[n - 1];
-        // return solve(nums, n - 1, dp);
+        return curr;
     }
     public int solve(int[] A, int n, int[] dp){
         if(n == 0)
