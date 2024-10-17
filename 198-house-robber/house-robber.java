445d1c4d3226f8339130902d5be8b7a3bc0cebce
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-10-17 09:42:49 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (26.08%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int rob(int[] nums) {
+        // Recursion, Memoization
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        // Tabulation: O(N)
+        dp[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            int taken = nums[i];
+            if(i > 1)
+                taken = nums[i] + dp[i - 2];
+            int notTaken = dp[i - 1];
+            dp[i] = Math.max(taken, notTaken);
+        }
+        return dp[n - 1];
+        // return solve(nums, n - 1, dp);
+    }
+    public int solve(int[] A, int n, int[] dp){
+        if(n == 0)
+            return A[0];
+        if(n < 0)
+            return 0;
+        if(dp[n] != -1)
+            return dp[n];
+        int taken = A[n] + solve(A, n - 2, dp);
+        int notTaken = solve(A, n - 1, dp);
+        
+        return dp[n] = Math.max(taken, notTaken);
+    }
+}
