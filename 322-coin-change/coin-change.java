7be1a5c5c486f8d87c9e6dabdc7c41ec9c207c3e
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-11-01 14:22:34 +0530

    Time: 114 ms (6.69%) | Memory: 44.9 MB (31.31%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,41 +1,67 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        int[] dp = new int[amount + 1];
         
-        Arrays.fill(dp, amount + 1);
-        dp[0] = 0;
-        for(int i = 1; i <= amount; i++){
-            for(int c: coins){
-                if(i - c >= 0){
-                    dp[i] = Math.min(dp[i], dp[i - c] + 1);
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for(int[] r: dp)
+            Arrays.fill(r, -1);
+    
+        for(int j = 0; j <= amount; j++){
+            if(j % coins[0] == 0)
+                dp[0][j] = j / coins[0];
+            else
+                dp[0][j] = (int) Math.pow(10, 9);
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= amount; j++){
+                int notTaken = dp[i - 1][j];
+                int taken = (int) Math.pow(10, 9);
+                if(coins[i] <= j){
+                    taken = 1 + dp[i][j - coins[i]];
                 }
+                dp[i][j] = Math.min(taken, notTaken);
             }
         }
         
-        return dp[amount] > amount ? -1 : dp[amount];
-//         int[] dp = new int[amount + 1];
-//         Arrays.fill(dp, -1);
-//         int res = recursion(coins, amount, dp);
-        
-//         return res != Integer.MAX_VALUE ? res : -1;
+        if(dp[n - 1][amount] == (int)Math.pow(10, 9))
+            return -1;
+        return dp[n - 1][amount];
+        // return memo(coins, n, amount, dp);
+        // return recursiveSolution(coins, coins.length - 1, amount);
     }
-    public int recursion(int[] coins, int amount, int[] dp){
-        if(amount == 0)
-            return 0;
-        if(amount < 0)
-            return Integer.MAX_VALUE;
-        if(dp[amount] != -1)
-            return dp[amount];
+    int memo(int[] coins, int n, int target, int[][] dp){
+        if(n == 0){
+            if(target % coins[0] == 0){
+                return target / coins[0];
+            }else{
+                return (int) Math.pow(10, 9);
+            }
+        }
+        if(dp[n][target] != -1)
+            return dp[n][target];
+        int notTaken = memo(coins, n - 1, target, dp);
+        int taken = (int) Math.pow(10, 9);
         
-        int minCoins = Integer.MAX_VALUE;
+        if(coins[n - 1] <= target){
+            taken = 1 + memo(coins, n, target - coins[n - 1], dp);
+        }
+        return Math.min(taken, notTaken);
+    }
+    int recursiveSolution(int[] coins, int n, int target){
+        if(n == 0){
+            if(target % coins[0] == 0){
+                return target / coins[0];
+            }else{
+                return (int)Math.pow(10, 9);
+            }
+        }
+        int notTaken = 0 + recursiveSolution(coins, n - 1, target);
+        int taken = (int)Math.pow(10, 9);
         
-        for(int c: coins){
-            int res = recursion(coins, amount - c, dp);
-            if(res != Integer.MAX_VALUE)
-                minCoins = Math.min(minCoins, res + 1);
-            
+        if(coins[n] <= target){
+            taken = 1 + recursiveSolution(coins, n, target - coins[n]);
         }
         
-        return dp[amount] = minCoins;
+        return Math.min(taken, notTaken);
     }
 }
