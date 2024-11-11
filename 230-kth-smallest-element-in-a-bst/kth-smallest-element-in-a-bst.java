525ac87b1c245da810c3356b430b3f2b07f6e75c
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-11-11 11:49:16 +0530

    Time: 1 ms (34.33%) | Memory: 44.8 MB (6.07%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -18,8 +18,24 @@ class Solution {
         // Bruteforce: O(N), O(N)
         ArrayList<Integer> ans = new ArrayList<>();
         inorder(root, ans);
+        //Optimized: O(N), O(1)
+        int[] finalAns = {Integer.MAX_VALUE};
+        int[] counter = {0};
+        inOrder(root, counter, finalAns, k);
         
-        return ans.get(k - 1);
+        return finalAns[0];
+        // return ans.get(k - 1);
+    }
+    public void inOrder(TreeNode root, int[] counter, int[] finalAns, int k){
+        if(root == null || counter[0] > k)
+            return;
+        inOrder(root.left, counter, finalAns, k);
+        counter[0] ++;
+        if(counter[0] == k){
+            finalAns[0] = root.val;
+            return;
+        }
+        inOrder(root.right, counter, finalAns, k);
     }
     public void inorder(TreeNode root, ArrayList<Integer> ans){
         if(root == null)
