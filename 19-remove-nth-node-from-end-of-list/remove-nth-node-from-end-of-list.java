4b1cdb4015184cd657ee9663a62928cb992f54f3
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2024-11-12 13:49:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (9.40%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -10,52 +10,50 @@
  */
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-        int sizeOfLL = size(head) - n;
-        if(sizeOfLL == 0){
-            head = head.next;
-            return head;
-        }
+//         int sizeOfLL = size(head) - n;
+//         if(sizeOfLL == 0){
+//             head = head.next;
+//             return head;
+//         }
             
-        ListNode temp = head;
-        System.out.println(sizeOfLL);
-        while(sizeOfLL > 1){
-            temp = temp.next;
-            sizeOfLL --;
-        }
-        
-        System.out.println(temp.val + " " + temp.next.val);
-        ListNode nextNode = temp.next;
-        // if(temp.next != null)
-        //     nextNode = temp.next;
-        temp.next = nextNode.next;
-        
-        return head;
-        
-        // Optimized Solution: O(n)
-//         ListNode fast = head, slow = head;
-        
-//         for(int i = 0; i < n; i++)
-//             fast = fast.next;
-//         if(fast == null)
-//             return head.next;
-        
-//         while(fast.next != null){
-//             slow = slow.next;
-//             fast = fast.next;
+//         ListNode temp = head;
+//         System.out.println(sizeOfLL);
+//         while(sizeOfLL > 1){
+//             temp = temp.next;
+//             sizeOfLL --;
 //         }
         
-//         slow.next = slow.next.next;
+//         System.out.println(temp.val + " " + temp.next.val);
+//         ListNode nextNode = temp.next;
+//         temp.next = nextNode.next;
         
 //         return head;
-    }
-    public int size(ListNode head){
-        int s = 0;
         
-        while(head != null){
-            s++;
-            head = head.next;
+        // Optimized Solution: O(n)
+        ListNode fast = head, slow = head;
+        
+        for(int i = 0; i < n; i++)
+            fast = fast.next;
+        if(fast == null)
+            return head.next;
+        
+        while(fast.next != null){
+            slow = slow.next;
+            fast = fast.next;
         }
         
-        return s;
+        slow.next = slow.next.next;
+        
+        return head;
     }
+//     public int size(ListNode head){
+//         int s = 0;
+        
+//         while(head != null){
+//             s++;
+//             head = head.next;
+//         }
+        
+//         return s;
+//     }
 }
