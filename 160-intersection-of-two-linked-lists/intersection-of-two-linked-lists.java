3956ef70fbbf7a7925e6163bab7f127f9be70f05
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2025-01-01 10:29:46 +0530

    Time: 1 ms (99.90%) | Memory: 48.9 MB (32.26%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -12,21 +12,33 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         // TC: O(len(headA) + len(headB)), SC: O(len(headA) + len(headB))
-        Set<ListNode> intersection = new HashSet<>();
-        ListNode tempHeadA = headA;
-        while(tempHeadA != null){
-            intersection.add(tempHeadA);
-            tempHeadA = tempHeadA.next;
-        }
+        // Set<ListNode> intersection = new HashSet<>();
+        // ListNode tempHeadA = headA;
+        // while(tempHeadA != null){
+        //     intersection.add(tempHeadA);
+        //     tempHeadA = tempHeadA.next;
+        // }
 
-        ListNode tempHeadB = headB;
-        while(tempHeadB != null){
-            if(intersection.contains(tempHeadB)){
-                return tempHeadB;
-            }
-            tempHeadB = tempHeadB.next;
-        }
+        // ListNode tempHeadB = headB;
+        // while(tempHeadB != null){
+        //     if(intersection.contains(tempHeadB)){
+        //         return tempHeadB;
+        //     }
+        //     tempHeadB = tempHeadB.next;
+        // }
 
+        // return null;
+        if(headA == null || headB == null)
         return null;
+
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        while(pA != pB){
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+
+        return pA;
     }
 }
