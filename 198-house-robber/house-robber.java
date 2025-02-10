d53ddcba01f83da3d8a030e78d1a139fe667cc0b
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2025-02-10 09:35:09 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (75.45%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,44 +1,53 @@
 class Solution {
     public int rob(int[] nums) {
-        // Recursion, Memoization
+        // Recursive sol: O(n^2)
+        // return recursiveSol(nums.length - 1, nums);
+        // Top down using dp: O(n)
         int n = nums.length;
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        // Tabulation: O(N)
-        // dp[0] = nums[0];
-        // for(int i = 1; i < n; i++){
-        //     int taken = nums[i];
-        //     if(i > 1)
-        //         taken = nums[i] + dp[i - 2];
-        //     int notTaken = dp[i - 1];
-        //     dp[i] = Math.max(taken, notTaken);
-        // }
-        // return dp[n - 1];
-        // return solve(nums, n - 1, dp);
-        
-        // Space optimized
-        int prev = 0, curr = nums[0];
+        // return memo(n - 1, nums, dp);
+        // Bottom up : O(N)
+        dp[0] = nums[0];
         for(int i = 1; i < n; i++){
-            int taken = nums[i];
-            if(i > 1)
-                taken = nums[i] + prev;
-            int notTaken = curr;
-            
-            prev = curr;
-            curr = Math.max(taken, notTaken);
+            int taken = nums[i], notTaken = dp[i - 1];
+            if(i > 1){
+                taken += dp[i - 2];
+            }
+            dp[i] = Math.max(taken, notTaken);
         }
-        return curr;
+        // [1, ]
+        return dp[n - 1];
     }
-    public int solve(int[] A, int n, int[] dp){
-        if(n == 0)
-            return A[0];
-        if(n < 0)
+    public int memo(int n, int[] nums, int[] dp){
+        if(n == 0){
+            return nums[0];
+        }
+        if(n < 0){
             return 0;
-        if(dp[n] != -1)
+        }
+        if(dp[n] != -1){
             return dp[n];
-        int taken = A[n] + solve(A, n - 2, dp);
-        int notTaken = solve(A, n - 1, dp);
-        
+        }
+        int taken = 0, notTaken = 0;
+        taken = nums[n] + memo(n - 2, nums, dp);
+        notTaken = memo(n - 1, nums, dp);
         return dp[n] = Math.max(taken, notTaken);
     }
+    public int recursiveSol(int n, int[] nums){
+        // max(recursiveSol(n - 1), recursiveSol(n - 2))
+        // if (n == 0) return nums[0], n < 0 -> 0
+        if(n == 0){
+            return nums[0];
+        }
+        if(n < 0){
+            return 0;
+        }
+        int take = 0, notTake = 0;
+        take = nums[n] + recursiveSol(n - 2, nums);
+        notTake = recursiveSol(n - 1, nums);
+        return Math.max(take, notTake);
+        
+            
+        }
 }
