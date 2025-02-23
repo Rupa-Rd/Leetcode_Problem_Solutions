7f7e7e60a541c57ce81fb8940666e651bc3c3e69
Rupa <102663541+Rupa-Rd@users.noreply.github.com>
2025-02-23 17:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (9.61%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -13,35 +13,50 @@ class Solution {
         if(head == null || head.next == null){
             return head;
         }
-        ArrayList<ListNode> odd = new ArrayList<>();
-        ArrayList<ListNode> even = new ArrayList<>();
-        int index = 0;
+        // ArrayList<ListNode> odd = new ArrayList<>();
+        // ArrayList<ListNode> even = new ArrayList<>();
+        // int index = 0;
 
-        while(head != null){
-            if(index % 2 == 0){
-                even.add(head);
-            }else{
-                odd.add(head);
-            }
-            head = head.next;
-            index++;
+        // while(head != null){
+        //     if(index % 2 == 0){
+        //         even.add(head);
+        //     }else{
+        //         odd.add(head);
+        //     }
+        //     head = head.next;
+        //     index++;
+        // }
+
+        // ListNode newHead = new ListNode(0);
+        // ListNode temp = newHead;
+
+        // for(ListNode node: even){
+        //     temp.next = node;
+        //     temp = temp.next;
+        // }
+
+        // for(ListNode node: odd){
+        //     temp.next = node;
+        //     temp = temp.next;
+        // }
+
+        // temp.next = null;
+
+        // return newHead.next;
+
+        ListNode evenHead = head;
+        ListNode oddHead = head.next;
+        ListNode even = head, odd = oddHead;
+
+        while(odd != null && odd.next != null){
+            even.next = odd.next;
+            even = even.next;
+            odd.next = even.next;
+            odd = odd.next;
         }
 
-        ListNode newHead = new ListNode(0);
-        ListNode temp = newHead;
+        even.next = oddHead;
 
-        for(ListNode node: even){
-            temp.next = node;
-            temp = temp.next;
-        }
-
-        for(ListNode node: odd){
-            temp.next = node;
-            temp = temp.next;
-        }
-
-        temp.next = null;
-
-        return newHead.next;
+        return evenHead;
     }
 }
